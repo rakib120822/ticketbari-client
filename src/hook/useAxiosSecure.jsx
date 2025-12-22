cd5e66c5b6340b4dd11e16f134af34dad875b571
rakib120822 <payelrakibulislam@gmail.com>
2025-12-22 17:11:27 +0600

    added vercel api

--- src/hook/useAxiosSecure.jsx
+++ src/hook/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from "./useAuth";
 import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://ticket-bari-server-nine.vercel.app",
 });
 
 function useAxiosSEcure() {
